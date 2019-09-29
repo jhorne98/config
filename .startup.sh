82ab79e015119c2f3efd2d9981e210f6bbf37e1f
Joel Horne <36707129+jhorne98@users.noreply.github.com>
2019-09-29 14:11:10 -0400

    updated startup script after like a year

--- .startup.sh
+++ .startup.sh
@@ -1,13 +1,17 @@
 # should be sourced in ~/.bashrc
 
 # aliases
-alias dwarves='cd ~/Downloads/df_linux && ./df && cd'
 alias ret='echo -e "Restarting network-manager... " && sudo service network-manager restart && echo -e "Done"'
 alias wp='cd && clear'
-alias cl='clear'
-alias current='cd ~/Documents/YCPS2So/CS320'
-alias inf='cd ~/Documents/Inform6/inform6Projects'
-alias dia='nano ~/Documents/diary.txt -t -r 191'
+alias cl='clear' #ctrl-l
+
+# quick dir pullup for semester
+alias cur='cd ~/Documents/YCPS1Se'
+alias scs='ssh jhorne@cscluster.ycp.edu'
+
+#alias inf='cd ~/Documents/Inform6/inform6Projects'
+alias dwf='cd ~/Documents/df_44_12_linux/df_linux && ./df && cd'
+alias dia='nano -t ~/Documents/diary.txt'
 alias src='source ~/.bashrc'
 
 # functions
@@ -15,11 +19,18 @@ function cs () {
 	cd "$@" && ls 
 }
 
-function ainst () {
+function agi () {
     sudo apt-get install "$@"
+}
+
+function ltyp () {
+	la | grep -E "\.${@}$"
 }
 
 # run commands
 if [[ ! $TERM =~ screen ]]; then
     exec tmux
 fi
+
+# getting sick of this tqbh
+#fortune -a | cowsay
