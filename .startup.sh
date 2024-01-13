d9b96c11373d8c1006cf5289e4f4e9f3fd4d2a5f
Joel Horne <jhorne@ycp.edu>
2024-01-13 13:15:03 -0500

    Add link to git-prompt

--- .startup.sh
+++ .startup.sh
@@ -1,11 +1,52 @@
 # should be sourced in ~/.bashrc
 
-# PS1 with current git branch
-parse_git_branch() {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+# store colors
+MAGENTA="\[\033[0;35m\]"
+YELLOW="\[\033[01;33m\]"
+BLUE="\[\033[00;34m\]"
+LIGHT_GRAY="\[\033[0;37m\]"
+CYAN="\[\033[0;36m\]"
+GREEN="\[\033[00;32m\]"
+RED="\[\033[01;31m\]"
+VIOLET='\[\033[01;35m\]'
+WHITE='\[\033[01;37m\]'
+ 
+function color_my_prompt {
+  local __user_and_host="$CYAN\u$LIGHT_GRAY@$RED\h"
+  local __cur_location="$GREEN\W"           # capital 'W': current directory, small 'w': full file path
+  local __git_branch_color="$RED"
+  local __prompt_tail="$VIOLET$"
+  local __user_input_color="$LIGHT_GRAY"
+  local __git_branch=$(__git_ps1); 
+  
+  # colour branch name depending on state
+  if [[ "${__git_branch}" =~ "*" ]]; then     # if repository is dirty
+      __git_branch_color="$RED"
+  elif [[ "${__git_branch}" =~ "$" ]]; then   # if there is something stashed
+      __git_branch_color="$YELLOW"
+  elif [[ "${__git_branch}" =~ "%" ]]; then   # if there are only untracked files
+      __git_branch_color="$LIGHT_GRAY"
+  elif [[ "${__git_branch}" =~ "+" ]]; then   # if there are staged files
+      __git_branch_color="$CYAN"
+  fi
+   
+  # Build the PS1 (Prompt String)
+  PS1="$WHITE[$__user_and_host $__cur_location$WHITE]$__git_branch_color$__git_branch $__prompt_tail$__user_input_color "
 }
-
-PS1="\[\033[01;32m\][\[\033[01;31m\]\u\[\033[01;32m\]@\h\[\033[01;37m\] \w\[\033[01;32m\]]\[\033[01;34m\]\$(parse_git_branch)\[\033[01;32m\]\$\[\033[00m\] "
+ 
+# configure PROMPT_COMMAND which is executed each time before PS1
+export PROMPT_COMMAND=color_my_prompt
+ 
+# if .git-prompt.sh exists, set options and execute it
+if [ -f ~/.git-prompt.sh ]; then
+  GIT_PS1_SHOWDIRTYSTATE=true
+  GIT_PS1_SHOWSTASHSTATE=true
+  GIT_PS1_SHOWUNTRACKEDFILES=true
+  GIT_PS1_SHOWUPSTREAM="auto"
+  GIT_PS1_HIDE_IF_PWD_IGNORED=true
+  GIT_PS1_SHOWCOLORHINTS=true
+  . ~/.git-prompt.sh
+fi
 
 # aliases
 alias wp='cd && clear'
