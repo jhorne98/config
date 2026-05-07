55ef2b6a62bc8aa7821b5dba4b95a8394d699e4d
Joel Horne <jhorne98@protonmail.com>
2026-05-06 21:22:35 -0400

    Update filepaths

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,19 @@ echo "Start Bootstrap"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo "Installed Rust"
 
-ln -s .startup.sh ~/.startup.sh
+ln -s $PWD/.startup.sh ~/.startup.sh
 echo ". ~/.startup.sh" >> ~/.bashrc
 echo "Installed startup script"
 
-ln -s .tmux.conf ~/.tmux.conf
+ln -s $PWD/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux && tmux source ~/.tmux.conf && exit
 echo "Installed tmux"
 
-ln -s .vimrc ~/.vimrc
+ln -s $PWD/.vimrc ~/.vimrc
 echo "Installed vim"
 
-ln -s .alacritty.toml ~/.alacritty.toml
-ln -s yt-dlp.conf ~/yt-dlp.conf
+ln -s $PWD/.alacritty.toml ~/.alacritty.toml
+ln -s $PWD/yt-dlp.conf ~/yt-dlp.conf
 
 echo "Installation finished"
